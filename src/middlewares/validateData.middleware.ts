d1e21d10f471c8efbdf7cde47091ccc309147e02
PauloMorenoD <paulomorenodeabreulima@hotmail.com>
2023-03-29 19:02:33 -0300

    feat: add sectors route

---
+++ src/middlewares/validateData.middleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express'
+import { ZodTypeAny } from "zod"
+
+const verifyDataIsValid = ( schema: ZodTypeAny) => async (req: Request, _res: Response, next: NextFunction): Promise<Response | void> => {
+    const validatedData = schema.parse(req.body)
+
+    req.body = validatedData
+
+    return next()
+}
+
+export default verifyDataIsValid
