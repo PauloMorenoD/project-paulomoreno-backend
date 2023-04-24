f8345a4caa407a94bed6f1d4918dec32043f05b8
PauloMorenoD <paulomorenodeabreulima@hotmail.com>
2023-04-24 18:01:09 -0300

    feat: project completed

--- src/controllers/users/getAllUsers.controller.ts
+++ src/controllers/users/getAllUsers.controller.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express'
 import { iReturnedUser } from '../../interfaces/users.interfaces'
 import getALlUsersService from '../../services/users/getAllUsers.services'
+import { Users } from '../../entities'
 
 const getAllUserController = async (_req: Request, res: Response): Promise<Response> => {
 
-    const users: iReturnedUser[] = await getALlUsersService()
+    const users: Users[] = await getALlUsersService()
 
     return res.status(200).json(users)
 }
