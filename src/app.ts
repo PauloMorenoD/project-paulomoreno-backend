5752db60aa0186d29ba2e08f41f10c692dd4a084
PauloMorenoD <paulomorenodeabreulima@hotmail.com>
2023-04-25 16:03:02 -0300

    feat : changing dev dependencies for render

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,13 @@ import userRoutes from "./routes/users.route";
 import loginRoutes from "./routes/login.route";
 import companieRoutes from "./routes/companies.route";
 import departmentsRoutes from "./routes/departments.route";
-
+const cors = require('cors');
 const app: Application = express();
 
 app.use(express.json())
+app.use(cors({
+    origin: 'https://api-empresas.onrender.com'
+}));
 
 app.use("/sectors", sectorRoutes)
 app.use("/users", userRoutes)
